4a267e33bc79dac88b106b77811781b6fa938720
Funky Gao <funky.gao@gmail.com>
2016-09-29 08:49:00 +0800

    demonstrate how to return a cleanup func to caller

--- helpers.go
+++ helpers.go
@@ -2,6 +2,7 @@ package assert
 
 import (
 	"io/ioutil"
+	"os"
 	"testing"
 )
 
@@ -13,4 +14,19 @@ func TempFile(t *testing.T) string {
 	}
 	f.Close()
 	return f.Name()
+}
+
+func Chdir(t *testing.T, dir string) (cleanup func()) {
+	old, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err = os.Chdir(dir); err != nil {
+		t.Fatal(err)
+	}
+
+	return func() {
+		os.Chdir(old)
+	}
 }
--- helpers_test.go
+++ helpers_test.go
@@ -1,10 +1,20 @@
 package assert
 
 import (
+	"os"
 	"testing"
 )
 
 func TestTempFile(t *testing.T) {
 	f := TempFile(t)
 	t.Log(f)
+}
+
+func TestChdir(t *testing.T) {
+	old, _ := os.Getwd()
+	Chdir(t, "/")()
+	//	defer Chdir(t, "/")()
+	cur, _ := os.Getwd()
+
+	Equal(t, old, cur)
 }
